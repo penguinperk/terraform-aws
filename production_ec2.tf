d0e30c59ad0b203e5ff62c3ab78b09db94ef6255
Scott <perk@penguinnorth.com>
2020-05-15 15:33:45 -0500

    adjusting the public key to work with Terraform Cloud (TC)

--- production_ec2.tf
+++ production_ec2.tf
@@ -1,18 +1,18 @@
 resource "aws_key_pair" "ec2key" {
-  key_name = "publicKey"
-  public_key = file(var.public_key_path)
+  key_name   = "publicKey"
+  public_key = var.public_key_scott
 }
 
 resource "aws_instance" "testInstance" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.subnet_public.id
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet_public.id
   vpc_security_group_ids = [aws_security_group.sg_22.id]
-  key_name = aws_key_pair.ec2key.key_name
-tags = {
-  Environment = var.environment_tag
-  Name = "terraform_Server1"
- }
+  key_name               = aws_key_pair.ec2key.key_name
+  tags = {
+    Environment = var.environment_tag
+    Name        = "terraform_Server1"
+  }
 }
 
 output "instance_ips" {
